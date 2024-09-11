e368e0350b5ed74b81dc033ed6df7fe392a1b5bd
Selim Ataballyev <selimdev00@gmail.com>
2024-09-11 13:58:21 +0500

    [init]: tailwind & scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ["~/assets/scss/main.scss"],
+});
